3a59a0b57f1d91ef4984d768ac43979039f8c21a
hizeer <47298056+hizeer@users.noreply.github.com>
2020-04-15 17:59:33 +0200

    tchat working well
    Next step:
    Add game with network

--- sylvain 15 04 2020/projets/ChatR.gd
+++ sylvain 15 04 2020/projets/ChatR.gd
@@ -4,31 +4,23 @@ var horaire= str(Time.hour)+":"+str(Time.minute)+":"+str(Time.second)
 var jour=(str(Time["day"])+ "/"+str(Time["month"])+ "/"+ str(Time["year"]))
 onready var edit = get_node("Panel/TextEdit")		
 onready var lab = get_node("Panel/ChatMain")
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("Panel").hide()
 	get_node("teteBox").hide()
 	get_node("Panel/ChatMain").set_text(jour+"\n")
 
-
-
-
 func _on_Button_pressed():
-	edit_text()	
 	send_text()	
 	
-remote func edit_text():
-	if (!edit.get_text().empty()):
-		lab.set_text(lab.get_text()+horaire+"/ " + gamestate.infos_joueur.nom +" : " + edit.get_text()+"\n")
-		edit.set_text(String())
+sync func edit_text(texte):
+	if (!texte.empty()):
+		lab.set_text(lab.get_text()+horaire+"/ " + gamestate.infos_joueur.nom +" : " + texte +"\n")
 		
-	
 func send_text():
-	if get_tree().is_network_server():
-		for id in network.players:
-			rpc(edit_text())
-
-
+	rpc("edit_text", edit.get_text())
+	edit.set_text(String())
 
 func _on_ButtonCroix_pressed():
 	if get_node("Panel").is_visible_in_tree():
--- sylvain 15 04 2020/projets/menu.gd
+++ sylvain 15 04 2020/projets/menu.gd
@@ -3,11 +3,8 @@ extends Control
 func _ready():
 	pass
 	
-
-
 func _on_Button_pressed():
 	get_tree().change_scene("res://rejoindre.tscn")
-
 
 func _on_Button2_pressed():
 	get_tree().change_scene("res://creer.tscn")
